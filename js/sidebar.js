11274a35d08e019b926733683d020227f036027f
root <fsalazar.sch@gmail.com>
2017-04-05 17:02:29 -0300

    primera subida

---
+++ js/sidebar.js
@@ -0,0 +1,28 @@
+<script type="text/javascript">
+var sideb ='<nav class="white" role="navigation">';
+    sideb +='<div class="nav-wrapper container">';
+    sideb +='<a id="logo-container" href="#" class="brand-logo"></a>';
+    sideb +='<ul class="right hide-on-med-and-down">';
+    sideb +='<li><a href="#"></a></li>';
+    sideb +='</ul>';
+
+    sideb +='<ul id="slide-out" class="side-nav">';
+    sideb +='<li><div class="userView">';
+    sideb +='<div class="background">';
+    sideb +='<img src="code.jpg" width="100%">';
+    sideb +='</div>';
+    sideb +='<a href="#!user"><img class="circle" src="foto.jpg"></a>';
+    sideb +='<a href="#!name"><span class="white-text name">Felipe Salazar Sch.</span></a>';
+    sideb +='<a href="maito:fsalazar.sch@gmail.com"><span class="white-text email">fsalazar.sch@gmail.com</span></a>';
+    sideb +='</div></li>';
+    sideb +='<li><a href="#!"><i class="material-icons">person</i>Antecedentes Personales</a></li>';
+    sideb +='<li><a href="#!">Second Link</a></li>';
+    sideb +='<li><div class="divider"></div></li>';
+    sideb +='<li><a class="subheader">Subheader</a></li>';
+    sideb +='<li><a class="waves-effect" href="#!">Third Link With Waves</a></li>';
+    sideb +='</ul>';
+    sideb +='<a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a>';
+    sideb +='</div></nav>';
+    alert(sideb);
+document.getElementById("sidebar").html(sideb);
+</script>
